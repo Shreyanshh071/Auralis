51ba3d689df33c9febc575217bdb394a3a684a59
Shreyansh <319473344+Shreyanshh071@users.noreply.github.com>
2026-09-01 20:10:23 +0530

    release: prepare official v1.0.0 release build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -38,8 +38,8 @@ android {
         applicationId = "com.auralis.music"
         minSdk = 24
         targetSdk = 35
-        versionCode = 4
-        versionName = "4.0.0"
+        versionCode = 1
+        versionName = "1.0.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
             useSupportLibrary = true
